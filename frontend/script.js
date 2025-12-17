3074b1c584f4fe70ca33596da382a606db67965b
hieuta1 <shoppecuahieuta@gmail.com>
2025-12-17 23:00:25 +0700

    cap nhat url cho frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/api/laptops")
+fetch("http://localhost:10000/api/laptops")
   .then(res => res.json())
   .then(data => {
     const tbody = document.getElementById("laptop-list");
